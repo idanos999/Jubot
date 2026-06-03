a42d1e4036b788d80c8235d0daa81490d9fdc2e0
idanos999 <113987298+idanos999@users.noreply.github.com>
2026-06-03 16:48:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Server is running!"
+
+@app.route("/transaction", methods=["POST"])
+def transaction():
+
+    data = request.json
+
+    print(data)
+
+    return jsonify({
+        "success": True,
+        "received": data
+    })
